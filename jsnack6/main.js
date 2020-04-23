ad302847ad6385d09e2e4a2a330c5008a9ca7360
Leticia <lsf@protonmail.ch>
2020-04-23 21:30:30 +0200

    jsnack6 true or false...

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,10 +1,11 @@
 //JSnack 6: Stampa le potenze di 2 fino a 1000
 
 //fare un ciclo che va da 1 a 1000;
+var range = [] ;
+
 for (var i = 1; i <= 1000; i++) {
     //console.log(i);
-    if (Math.log2()) {
+    var numeroCorrente = range [i];
+    var potDue = (Math.log2(numeroCorrente) % 1 == 0);
         console.log(i);
-    }
-
 }
