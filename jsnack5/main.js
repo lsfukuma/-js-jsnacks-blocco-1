678de87d0bcf4858d8b33fd9ebdbff5c22c2d9ed
Leticia <lsf@protonmail.ch>
2020-04-23 14:59:01 +0200

    JSnack5

---
+++ jsnack5/main.js
@@ -0,0 +1,19 @@
+//JSnack 5: Stampa il cubo dei primi N numeri, dove N è un numero indicato dall'utente
+
+
+//chiedere all'utente un numero
+var arrayNumber = [] ;
+var primiN = 0
+
+var number = parseInt(prompt('Digitare un numero'));
+console.log('Numero scelto dall\'utente: ' + number);
+
+
+//fare un ciclo in cui otteniamo i primi N numeri
+for (var i = 0; i < number; i++) {
+    //elencare i primi N numeri
+    primiN = arrayNumber.push(number);
+    //stampo i primi N numeri: console.log(primiN);
+    //Devo fare il cubo di OGNI numero
+    console.log(Math.pow(primiN, 3))
+}
---
+++ jsnack6/main.js
@@ -0,0 +1,3 @@
+//JSnack 6: Stampa le potenze di 2 fino a 1000
+
+alert('ciao')
