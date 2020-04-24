21d4b42acf99cb209228bdcc439864cd1845aa16
Leticia <lsf@protonmail.ch>
2020-04-24 13:16:21 +0200

    jsnack6 finito

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -3,6 +3,9 @@
 //fare un ciclo che va da 1 a 1000;
 for (var i = 1; i <= 1000; i++) {
     //console.log(i);
-    var potDue = Math.log2(i) % 1 === 0;
-        console.log(potDue == true);
+    var potDue = Math.log2(i) % 1;
+    if (potDue == 0) {
+        //il numero è potenza di 2 (perche il risultato è un numero intero), allora lo stampo
+        console.log(i);
+    }
 }
